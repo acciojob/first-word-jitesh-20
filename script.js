055a9ebe8e954082beface0d23f6081e37947ee2
Jitesh Kumar <105545377+jitesh-20@users.noreply.github.com>
2025-01-21 13:02:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
 function firstWord(s) {
   // your code here
+	s=s.trim()
+	let str=s.split(" ")
+	return str[0];
 }
 
 // Do not change the code below
 
 const s = prompt("Enter String:");
 alert(firstWord(s));
+ 
